9451d546f7e32cee751bdc3a9be83cc59298d891
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-09-02 14:51:57 +0530

    rollback

--- grid.ts
+++ grid.ts
@@ -11,80 +11,66 @@ async function downloadImage(url: string): Promise<Buffer> {
   return Buffer.from(response.data, "binary");
 }
 
-async function getImage(imagePath: string): Promise<Jimp> {
-  let image: Jimp;
-
-  if (imagePath.startsWith("http://") || imagePath.startsWith("https://")) {
-    const imageBuffer = await downloadImage(imagePath);
-    image = await Jimp.read(imageBuffer);
-  } else {
-    image = await Jimp.read(imagePath);
-  }
-
-  return image;
-}
-
-async function processAndSaveSlice(
-  image: Jimp,
-  outputRootFolder: string,
-  originalImageName: string,
-  i: number,
-  j: number
-): Promise<string> {
-  const width = image.getWidth();
-  const height = image.getHeight();
-  const sliceWidth = Math.floor(width / 2);
-  const sliceHeight = Math.floor(height / 2);
-
-  if (sliceWidth <= 0 || sliceHeight <= 0) {
-    throw new Error("Invalid image dimensions");
-  }
-
-  const slice = image
-    .clone()
-    .crop(i * sliceWidth, j * sliceHeight, sliceWidth, sliceHeight);
-
-  const variantFolder = `variant_${i}_${j}`;
-  const variantFolderPath = path.join(outputRootFolder, variantFolder);
-  const sliceFilename = `${originalImageName}.png`;
-  const sliceFilePath = path.join(variantFolderPath, sliceFilename);
-
-  // Create the variant folder if it doesn't exist
-  await mkdir(variantFolderPath, { recursive: true });
-
-  // Save the slice image inside the variant folder
-  await slice.writeAsync(sliceFilePath);
-  return sliceFilePath;
-}
-
 async function sliceImage(imagePath: string): Promise<string[]> {
   try {
-    const outputRootFolder = "./output"; // Specify the root output folder path
-    const originalImageName = path.basename(imagePath, path.extname(imagePath));
+    let image: Jimp;
 
-    const image = await getImage(imagePath);
+    if (imagePath.startsWith("http://") || imagePath.startsWith("https://")) {
+      const imageBuffer = await downloadImage(imagePath);
+      image = await Jimp.read(imageBuffer);
+    } else {
+      image = await Jimp.read(imagePath);
+    }
 
-    if (
-      image.getWidth() !== 1856 ||
-      image.getHeight() !== 2464 ||
-      image.getHeight() === image.getWidth()
-    ) {
+    const width = image.getWidth();
+    const height = image.getHeight();
+
+    if (width === height || width !== 1856 || height !== 2464) {
       console.log(
-        `Skipping ${path.basename(imagePath)} due to incorrect dimensions.`
+        `Skipping ${path.basename(
+          imagePath
+        )} due to incorrect dimensions.\n\nheight: ${height}\nwidth: ${width}`
       );
       return [];
     }
+    const sliceWidth = Math.floor(width / 2);
+    const sliceHeight = Math.floor(height / 2);
 
-    const slicesPromises: Promise<string>[] = [];
+    console.log("Image Dimensions:", width, "x", height);
+    console.log("Slice Dimensions:", sliceWidth, "x", sliceHeight);
+
+    if (sliceWidth <= 0 || sliceHeight <= 0) {
+      throw new Error("Invalid image dimensions");
+    }
+
+    const outputRootFolder = "./output"; // Specify the root output folder path
+    const originalImageName = path.basename(imagePath, path.extname(imagePath));
+
+    // Generate and save each image slice
+    const slices: string[] = [];
     for (let i = 0; i < 2; i++) {
       for (let j = 0; j < 2; j++) {
-        slicesPromises.push(
-          processAndSaveSlice(image, outputRootFolder, originalImageName, i, j)
-        );
+        const slice = image
+          .clone()
+          .crop(i * sliceWidth, j * sliceHeight, sliceWidth, sliceHeight);
+
+        const variantFolder = `variant_${i}_${j}`;
+        const variantFolderPath = path.join(outputRootFolder, variantFolder);
+        const sliceFilename = `sliced_${i}_${j}_${originalImageName}.jpg`;
+        const sliceFilePath = path.join(variantFolderPath, sliceFilename);
+
+        // Create the variant folder if it doesn't exist
+        await mkdir(variantFolderPath, { recursive: true });
+
+        // Save the slice image inside the variant folder
+        await slice.writeAsync(sliceFilePath);
+        slices.push(sliceFilePath);
       }
     }
 
-    const slices = await Promise.all(slicesPromises);
+    // const jsonData = JSON.stringify(slices);
+
+    // await writeFile(path.join(outputRootFolder, "slices.json"), jsonData);
     console.log("Image slices saved to the output folders.");
     return slices;
   } catch (error) {
@@ -92,15 +78,17 @@ async function sliceImage(imagePath: string): Promise<string[]> {
   }
 }
 
-// Example usage with multiple remote image URLs in parallel
-Promise.race(remotePaths.map(sliceImage))
-  .then((results) => {
-    console.log(`Processing: `, sliceImage);
-    for (const slices of results) {
+// Example usage with a remote image URL
+const remoteImagePath =
+  "https://media.discordapp.net/attachments/1120340802879631400/1136593116028219483/merise_macmerise_Electric_blue_octopus_strumming_a_guitar_on_a__14c515cc-9e82-4ce9-b488-53959dafa4ca.png";
+
+for (let i = 500; i <= 510; i++) {
+  sliceImage(remotePaths[i])
+    .then((slices) => {
       console.log("Image slices:");
       console.log(slices);
-    }
-  })
-  .catch((err) => {
-    console.error("Error:", err);
-  });
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+    });
+}
